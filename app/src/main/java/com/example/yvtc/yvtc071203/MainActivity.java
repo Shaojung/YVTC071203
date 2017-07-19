b6fbf139211f245dda59d756796722fb585838ac
Shaojung <shaojung@gmail.com>
2017-07-19 08:42:19 +0800

    ContextMenu

--- app/src/main/java/com/example/yvtc/yvtc071203/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc071203/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.yvtc.yvtc071203;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        registerForContextMenu(tv);
     }
 
     @Override
@@ -37,5 +42,22 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Click Update", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add("AA");
+        menu.add("BBBBB");
+        menu.add("CCCC");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getTitle().equals("AA"))
+        {
+            Toast.makeText(MainActivity.this, "AA Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onContextItemSelected(item);
     }
 }
